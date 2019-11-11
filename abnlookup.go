cdbec7189c48d386353e1ff5f6ac4eaab49f1ea0
Conrad Pankoff <deoxxa@fknsrs.biz>
2019-11-12 07:01:51 +1100

    code import

---
+++ abnlookup.go
@@ -0,0 +1,88 @@
+package abnlookup
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/pkg/errors"
+)
+
+var (
+	ErrWrongLength     = errors.New("abn should be eleven digits")
+	ErrInvalidFormat   = errors.New("abn should only be numbers")
+	ErrInvalidChecksum = errors.New("invalid checksum")
+	ErrInvalidABN      = errors.New("invalid abn format")
+	ErrRecordNotFound  = errors.New("record not found")
+	ErrUnknownResponse = errors.New("unknown response from ABR")
+)
+
+func Validate(abn string) error {
+	if len(abn) != 11 {
+		return ErrWrongLength
+	}
+
+	for i := 0; i < len(abn); i++ {
+		if abn[i] < '0' || abn[i] > '9' {
+			return ErrInvalidFormat
+		}
+	}
+
+	var n int
+
+	n += int(abn[0]-'1') * 10
+	n += int(abn[1]-'0') * 1
+	n += int(abn[2]-'0') * 3
+	n += int(abn[3]-'0') * 5
+	n += int(abn[4]-'0') * 7
+	n += int(abn[5]-'0') * 9
+	n += int(abn[6]-'0') * 11
+	n += int(abn[7]-'0') * 13
+	n += int(abn[8]-'0') * 15
+	n += int(abn[9]-'0') * 17
+	n += int(abn[10]-'0') * 19
+
+	if n%89 != 0 {
+		return ErrInvalidChecksum
+	}
+
+	return nil
+}
+
+func Lookup(abn string) error {
+	if err := Validate(abn); err != nil {
+		return errors.Wrap(err, "abnlookup.Lookup: preliminary validation failed")
+	}
+
+	res, err := http.Get("https://abr.business.gov.au/ABN/View?abn=" + abn)
+	if err != nil {
+		return errors.Wrap(err, "abnlookup.Lookup: couldn't perform request")
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.Errorf("abnlookup.Lookup: invalid response code; expected 200 OK but got %s", res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		return errors.Wrap(err, "abnlookup.Lookup: couldn't parse response")
+	}
+	if doc == nil {
+		return errors.Errorf("abnlookup.Lookup: couldn't parse response")
+	}
+
+	if strings.Contains(doc.Find("div.process-message").Text(), "No record found matching ABN") {
+		return ErrRecordNotFound
+	}
+
+	if strings.Contains(doc.Find("div.process-message").Text(), "The number entered is not a valid ABN") {
+		return ErrInvalidABN
+	}
+
+	if strings.HasPrefix(doc.Find("title").Text(), "Current details for ABN") {
+		return nil
+	}
+
+	return ErrUnknownResponse
+}
---
+++ abnlookup_test.go
@@ -0,0 +1,61 @@
+package abnlookup
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type abnTest struct {
+	abn  string
+	err1 error
+	err2 error
+}
+
+var (
+	tests = []abnTest{
+		{"87 007 382 031", ErrWrongLength, nil},
+		{"87007382xxx", ErrInvalidFormat, nil},
+		{"87007382031", ErrInvalidChecksum, nil},
+		{"87007382032", nil, nil},
+		{"87007382033", ErrInvalidChecksum, nil},
+		{"51824753556", nil, nil},
+		{"51824999396", nil, ErrRecordNotFound},
+	}
+)
+
+func TestValidate(t *testing.T) {
+	for _, e := range tests {
+		t.Run(e.abn, func(t *testing.T) {
+			if e.err1 != nil {
+				assert.Contains(t, Validate(e.abn).Error(), e.err1.Error())
+			} else {
+				assert.NoError(t, Validate(e.abn))
+			}
+		})
+	}
+}
+
+func TestLookup(t *testing.T) {
+	for _, e := range tests {
+		t.Run(e.abn, func(t *testing.T) {
+			if e.err1 != nil {
+				assert.Contains(t, Lookup(e.abn).Error(), e.err1.Error())
+			} else if e.err2 != nil {
+				assert.Contains(t, Lookup(e.abn).Error(), e.err2.Error())
+			} else {
+				assert.NoError(t, Lookup(e.abn))
+			}
+		})
+	}
+}
+
+func BenchmarkValidate(b *testing.B) {
+	for _, e := range tests {
+		b.Run(e.abn, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Validate(e.abn)
+			}
+		})
+	}
+}
